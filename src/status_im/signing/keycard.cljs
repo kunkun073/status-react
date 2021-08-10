9a433a08d9bc80166a8491a0a915f422e93fb6fc
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2021-08-10 12:00:06 +0300

    fix tx sending with kk

--- src/status_im/signing/keycard.cljs
+++ src/status_im/signing/keycard.cljs
@@ -26,15 +26,23 @@
 
 (defn prepare-transaction
   [{:keys [gas gasPrice data nonce tx-obj]}]
-  (let [{:keys [from to value chat-id message-id command?]} tx-obj]
+  (let [{:keys [from to value chat-id message-id command? maxPriorityFeePerGas maxFeePerGas]} tx-obj]
     (cond-> {:from       from
              :to         to
              :value      value
-             :gas        (str "0x" (abi-spec/number-to-hex gas))
-             :gasPrice   (str "0x" (abi-spec/number-to-hex gasPrice))
              :chat-id    chat-id
              :message-id message-id
              :command?   command?}
+      maxPriorityFeePerGas
+      (assoc :maxPriorityFeePerGas (str "0x" (abi-spec/number-to-hex
+                                              (js/parseInt maxPriorityFeePerGas))))
+      maxFeePerGas
+      (assoc :maxFeePerGas (str "0x" (abi-spec/number-to-hex
+                                      (js/parseInt maxFeePerGas))))
+      gas
+      (assoc :gas (str "0x" (abi-spec/number-to-hex gas)))
+      gasPrice
+      (assoc :gasPrice (str "0x" (abi-spec/number-to-hex gasPrice)))
       data
       (assoc :data data)
       nonce
@@ -93,7 +101,7 @@
 (fx/defn sign-with-keycard
   {:events [:signing.ui/sign-with-keycard-pressed]}
   [{:keys [db] :as cofx}]
-  (let [message (get-in db [:signing/tx :message])]
+  (let [{:keys [message maxPriorityFeePerGas maxFeePerGas]} (get db :signing/tx)]
     (fx/merge
      cofx
      {:db (-> db
